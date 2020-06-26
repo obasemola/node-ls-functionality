cb63af72f1afd63c2d2418b04dd23926894b85c9
Ayodele obasemola <obasemolaayodele@gmail.com>
2020-06-26 16:32:43 +0300

    nls functionality added with different approaches and refactoring

--- index.js
+++ index.js
@@ -5,21 +5,44 @@
 const fs = require('fs');
 const util = require('util');
 
-//USING PROMISES THROUGH THE NODE UTILITIES DOCUMENTATION
+//3. USING PROMISES THROUGH THE NODE UTILITIES DOCUMENTATION
 // const lstat = util.promisify(fs.lstat);
 
-//USING PROMISES THROUGH THE NODE FS PROMISES DOCUMENTATION
+//4. USING PROMISES THROUGH THE NODE FS PROMISES DOCUMENTATION
 const lstat = fs.promises.lstat;
 
 //OR
 
 // const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), (err, filenames) => {
+fs.readdir(process.cwd(), async (err, filenames) => {
   if (err) {
     console.log(err);
   }
 
+  const statPromises = filenames.map((filename) => {
+    return lstat(filename);
+  });
+
+  const allStats = await Promise.all(statPromises);
+
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
+
+    console.log(filenames[index], stats.isFile());
+  }
+
+
+  // for (let filename of filenames) {
+  //   try {
+  //     const stats = await lstat(filename);
+
+  //   console.log(filename, stats.isFile());
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+    
+  // }
 
   //1. NOT THE BEST WAY TO WRITE WRITE THE CODE AS THIS ALLOWS FOR INCONSISTENT ARRANGEMENT AND ORDER OF FILENAMES WHEN NLS IS CALLED
 
