2e746f51527d7c224727215e229a741cc9f5975e
Ayodele obasemola <obasemolaayodele@gmail.com>
2020-06-26 16:52:25 +0300

    added a statement to bolden item names depending on whether or not they are folders or files

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 const fs = require('fs');
 const util = require('util');
+const chalk = require('chalk');
 
 //3. USING PROMISES THROUGH THE NODE UTILITIES DOCUMENTATION
 // const lstat = util.promisify(fs.lstat);
@@ -29,7 +30,11 @@ fs.readdir(process.cwd(), async (err, filenames) => {
   for (let stats of allStats) {
     const index = allStats.indexOf(stats);
 
-    console.log(filenames[index], stats.isFile());
+    if (stats.isFile()) {
+      console.log(filenames[index]);
+    } else{
+      console.log(chalk.bold(filenames[index]));
+    }
   }
 
 
